b246f640e4715fd50dedb97da02b447016ec23b9
zhiluniao <huangshunle_gxu@163.com>
2019-07-17 00:29:58 +0800

    第一个未分离的完整版本

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,61 @@
 use std::env;
-use std::fs;
+use std::process;
+
+pub mod minigrep{
+
+    use std::fs;
+    use std::error::Error;
+    use std::result::Result;
+
+    pub struct Config{
+        pub query:String,
+        pub filename:String,
+    }
+
+    impl Config{
+        pub fn new(args : &[String]) -> Result<Config,&'static str>{
+            if args.len() < 3{
+                return Err("Not enough arguments");
+            }
+            let query = args[1].clone();
+            let filename = args[2].clone();
+            Ok( Config{ query, filename })
+        }
+
+        pub fn run(config:Config)-> Result<(),Box<dyn Error>>{
+            let contents= fs::read_to_string(config.filename)?;
+            println!("With text:\n{}",contents);
+            Ok(())
+        }
+    }
+
+
+}
 
 fn main() {
     let args : Vec<String> = env::args().collect();
     println!("{:?}",args);
 
-    let (query,filename) = parse_config(&args);
+//    let config = parse_config(&args);
 
-    println!("Searching for {}",query);
-    println!("In file {}",filename);
+    let result = minigrep::Config::new(&args);
 
-    let contents= fs::read_to_string(filename).expect("Something went wrong reading the file");
-    println!("With text:\n{}",contents);
+    let _result = match result {
+      Ok(_config) => {
+          println!("Searching for {}",_config.query);
+          println!("In file {}",_config.filename);
+
+          if let Err(e) = minigrep::Config::run(_config){
+              println!("Application error: {}",e);
+
+              process::exit(1);
+          }
+
+      },
+        Err(_error) =>{
+            panic!("There are some errors")
+        }
+    };
+
 }
-fn parse_config(args : &[String]) -> (&str,&str){
-    let query = &args[1];
-    let filename = &args[2];
-    (query,filename)
-}
+
